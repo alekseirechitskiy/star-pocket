d6048a291c305c3523e4eee2d0d05e3eabea43c0
alexeirechitskiy <slowspirit@gmail.com>
2023-01-10 17:14:23 +0300

    improved media for containers, added smooth scroll for 'contct us'

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -27,17 +27,23 @@ export default function () {
     }
   );
 
-  // Исчезновение header при скролле
-  const header = document.querySelector('.header');
-  let scrollPrev = 0;
+  // Плавный переход по якорям
 
-  window.onscroll = function () {
-    let scrolled = window.pageYOffset;
-    if (scrolled > 200 && scrolled > scrollPrev) {
-      header.classList.add('header--out');
-    } else {
-      header.classList.remove('header--out');
-    }
-    scrollPrev = scrolled;
-  };
+  document.querySelectorAll('a[href^="#"').forEach(link => {
+    link.addEventListener('click', function (evt) {
+      evt.preventDefault();
+
+      let href = this.getAttribute('href').substring(1);
+      const scrollTarget = document.getElementById(href);
+      // const topOffset = header.offsetHeight; // если не нужен отступ сверху равный высоте header
+      const topOffset = 0; // если не нужен отступ сверху 
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - topOffset;
+
+      window.scrollBy({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    });
+  });
 };
